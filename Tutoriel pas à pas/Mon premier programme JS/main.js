179721d336a7b16a40c10c316772347e13dc7e0f
Hidhem <hassaini.mehdi@hotmail.fr>
2026-03-20 07:03:07 +0100

    first commit

---
+++ Tutoriel pas à pas/Mon premier programme JS/main.js
@@ -0,0 +1,15 @@
+let entreeJoueur = document.getElementById("inputEcriture");
+    console.log(entreeJoueur);
+
+let boutonValidation = document.getElementById("btnValiderMot");
+    console.log(boutonValidation);
+
+let motAfficher = document.querySelector(".zonePropositions");
+    console.log(motAfficher);
+
+let scoreAfficher = document.querySelector(".zoneScore span");
+    console.log(scoreAfficher);
+
+let choixJoueur = document.querySelectorAll(".optionSource input")
+    console.log(choixJoueur)
+
